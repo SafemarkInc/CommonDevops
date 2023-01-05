27689c30046d217b3168550e6b1ec1eb41f95e61
Michael Buchoff <mbuchoff@gmail.com>
2023-01-05 16:41:28 -0500

    Moved provider into top level files

--- Korea/korea.tf
+++ Korea/korea.tf
@@ -3,24 +3,19 @@ variable "AZURE_CLIENTID" { type = string }
 variable "AZURE_TENANTID" { type = string }
 variable "AZURE_CLIENTSECRET" { type = string }
 
-variable "STATEFILE_STORAGEACCOUNTNAME" { type = string }
-variable "STATEFILE_CONTAINERNAME" { type = string }
-variable "STATEFILE_KEY" { type = string }
-
 terraform {
-  backend "azurerm" {
-    storage_account_name = var.STATEFILE_STORAGEACCOUNTNAME
-    container_name       = var.STATEFILE_CONTAINERNAME
-    key                  = var.STATEFILE_KEY
-  }
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.AZURE_SUBSCRIPTIONID
+  client_id       = var.AZURE_CLIENTID
+  tenant_id       = var.AZURE_TENANTID
+  client_secret   = var.AZURE_CLIENTSECRET
 }
 
 module "all_resources" {
   source   = "../../Terraform"
   LOCATION = "koreacentral"
-
-  AZURE_SUBSCRIPTIONID = var.AZURE_SUBSCRIPTIONID
-  AZURE_CLIENTID       = var.AZURE_CLIENTID
-  AZURE_TENANTID       = var.AZURE_TENANTID
-  AZURE_CLIENTSECRET   = var.AZURE_CLIENTSECRET
 }
--- US/us.tf
+++ US/us.tf
@@ -3,19 +3,19 @@ variable "AZURE_CLIENTID" { type = string }
 variable "AZURE_TENANTID" { type = string }
 variable "AZURE_CLIENTSECRET" { type = string }
 
-variable "STATEFILE_STORAGEACCOUNTNAME" { type = string }
-variable "STATEFILE_CONTAINERNAME" { type = string }
-
 terraform {
   backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.AZURE_SUBSCRIPTIONID
+  client_id       = var.AZURE_CLIENTID
+  tenant_id       = var.AZURE_TENANTID
+  client_secret   = var.AZURE_CLIENTSECRET
 }
 
 module "all_resources" {
   source   = "../../Terraform"
   LOCATION = "centralus"
-
-  AZURE_SUBSCRIPTIONID = var.AZURE_SUBSCRIPTIONID
-  AZURE_CLIENTID       = var.AZURE_CLIENTID
-  AZURE_TENANTID       = var.AZURE_TENANTID
-  AZURE_CLIENTSECRET   = var.AZURE_CLIENTSECRET
 }
